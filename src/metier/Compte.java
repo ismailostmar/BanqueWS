1ed064af0a4835ec262c39fbd67762ea75a45e6a
DEVGEEKTM <ismailostmar@gmail.com>
2021-11-03 02:35:04 +0100

    Add Annotation.

--- src/metier/Compte.java
+++ src/metier/Compte.java
@@ -1,10 +1,22 @@
 package metier;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 import java.util.Date;
 
+/* Transformer un Objet en XML : il va etre converti
+    en un element XML qui s'appelle Compte
+* */
+@XmlRootElement(name = "compte")
+// les annotations sont dans les attributs
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Compte {
     private Long code;
     private double solde;
+    // Annotation JAX-Binding , l'attribut n'est pas serializable Ignoré
+    @XmlTransient
     private Date dateCreation;
 
     public Compte(){}
